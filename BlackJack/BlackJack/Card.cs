81e7fde3d9bec8fe7e05dae156c4f17805ff9917
Victor <lolwner@gmail.com>
2017-01-31 21:12:22 +0200

    Card and deck implementation

---
+++ BlackJack/BlackJack/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Card
+    {
+        public string Name; 
+        public string Suit; //масть
+        public int Value; //очки карты
+
+        public Card(string Name, string Suit, int Value)
+        {
+            this.Name = Name;
+            this.Suit = Suit;
+            this.Value = Value;
+        }
+    }
+}
---
+++ BlackJack/BlackJack/Deck.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    public class Deck 
+    {
+        public List<Card> deck; 
+
+        public Deck()
+        {
+            deck = new List<Card>();
+
+            for (int i = 0; i <= 8; i++)
+            {
+                string name = i+2.ToString();
+                deck.Add(new BlackJack.Card(name, "Clubs", i+2));
+            }
+            deck.Add(new Card("Ace", "Clubs", 11));
+            deck.Add(new Card("King", "Clubs", 10));
+            deck.Add(new Card("Queen", "Clubs", 10));
+            deck.Add(new Card("Jack", "Clubs", 10));
+
+            for (int i = 0; i <= 8; i++)
+            {
+                string name = i + 2.ToString();
+                deck.Add(new BlackJack.Card(name, "Spades", i + 2));
+            }
+            deck.Add(new Card("Ace", "Spades", 11));
+            deck.Add(new Card("King", "Spades", 10));
+            deck.Add(new Card("Queen", "Spades", 10));
+            deck.Add(new Card("Jack", "Spades", 10));
+
+            for (int i = 0; i <= 8; i++)
+            {
+                string name = i + 2.ToString();
+                deck.Add(new BlackJack.Card(name, "Hearts", i + 2));
+            }
+            deck.Add(new Card("Ace", "Hearts", 11));
+            deck.Add(new Card("King", "Hearts", 10));
+            deck.Add(new Card("Queen", "Hearts", 10));
+            deck.Add(new Card("Jack", "Hearts", 10));
+
+            for (int i = 0; i <= 8; i++)
+            {
+                string name = i + 2.ToString();
+                deck.Add(new BlackJack.Card(name, "Diamonds", i + 2));
+            }
+            deck.Add(new Card("Ace", "Diamonds", 11));
+            deck.Add(new Card("King", "Diamonds", 10));
+            deck.Add(new Card("Queen", "Diamonds", 10));
+            deck.Add(new Card("Jack", "Diamonds", 10));
+
+        }
+        
+        private static Random rng = new Random(); 
+
+        public static void Shuffle(List<Card> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+
+        public Card PickCard()
+        {
+            int k = rng.Next(0, deck.Count);
+            var card = deck[k];
+            return card;
+        }
+
+    }
+}
