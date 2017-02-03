4959cf3f912aada3ed5753550397b4a66ac23511
Victor <lolwner@gmail.com>
2017-02-03 17:53:50 +0200

    Old game implementation deleted

--- BlackJack/BlackJack/Game.cs
+++ BlackJack/BlackJack/Game.cs
@@ -20,70 +20,7 @@ namespace BlackJack
             MSG.ShowCard(card);
             return score;
         }
-
-        public int NewGame()
-        {
-            Deck deck = new Deck();
-            deck.InitializeDeck();
-            Console.WriteLine("Player`s card");
-            PlayerScore += TakeCard(deck);
-            PlayerScore += TakeCard(deck);
-            MSG.ShowScore(PlayerScore);
-            Console.WriteLine("Player score is {0}", PlayerScore);
-
-            Console.WriteLine("Casino card");
-            CasinoScore += TakeCard(deck);
-            CasinoScore += TakeCard(deck);
-            Console.WriteLine("Casino score is {0}", CasinoScore);
-
-            while (PlayerScore < 21)
-            {
-                if (PlayerScore == 21)
-                {
-                    Console.WriteLine("BlackJack, player win");
-                    return 0;
-                }
-                if (CasinoScore == 21)
-                {
-                    Console.WriteLine("BlackJack, casino win");
-                    return 0;
-                }
-
-                Console.WriteLine("Take card? Y/N");
-                string choise = Console.ReadLine().ToString();
-                if (choise == "y" || choise == "Y")
-                {
-                    Console.WriteLine("Player`s card");
-                    PlayerScore += TakeCard(deck);
-                    Console.WriteLine("Player score is {0}", PlayerScore);
-                    if (PlayerScore > 21)
-                    {
-                        Console.WriteLine("Too many, casino win");
-                        return 0;
-                    }
-                    Console.WriteLine("Casino card");
-                    CasinoScore += TakeCard(deck);
-                    Console.WriteLine("Casino score is {0}", CasinoScore);
-                    if (CasinoScore > 21)
-                    {
-                        Console.WriteLine("Casino has too many, player win");
-                        return 0;
-                    }
-                }
-                else if (choise == "n" || choise == "N")
-                {
-                    CasinoScore += TakeCard(deck);
-                    Console.WriteLine("Casino score is {0}", CasinoScore);
-                    return 0;
-                }
-                else
-                {
-                    Console.WriteLine("Choose Y or N");
-                }
-
-            }
-            return 0;
-        }
+        
 
         public void Play()
         {
